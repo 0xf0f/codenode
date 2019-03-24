2bb5a6069295d2b7f5a9f2126a11c96e0ea33f54
0xf0f <0x0meta@gmail.com>
2019-03-25 08:19:18 +1300

    Fixed python.Method

--- codenode/python/nodes/method.py
+++ codenode/python/nodes/method.py
@@ -3,7 +3,4 @@ from .function_definition import Function
 
 class Method(Function):
     def __init__(self, name, *args, **kwargs):
-        super().__init__(name, 'self')
-
-        self.args.extend(args)
-        self.kwargs.update(kwargs)
+        super().__init__(name, ('self', *args), **kwargs)
