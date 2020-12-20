e30db85469898b21f2252fbe815c5b9ba0993237
0xf0f <0x0meta@gmail.com>
2020-12-21 00:00:32 +1300

    upping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codenode',
-    version='0.6',
+    version='0.7',
     packages=find_packages(),
     url='https://github.com/0xf0f/codenode',
     license='MIT',
