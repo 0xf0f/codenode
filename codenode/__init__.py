5673c4f6caa39aa52b6dff01b762949058a97c06
0xf0f <0x0meta@gmail.com>
2021-03-30 20:32:50 +1300

    remove Writer from package imports

--- codenode/__init__.py
+++ codenode/__init__.py
@@ -3,7 +3,6 @@ from .node import Line
 from .block import Block
 from .empty_lines import EmptyLines
 from .file import File
-from .writer import CodeNodeWriter
 
 default_indent = '    '
 default_writer = CodeNodeWriter()
