cc94c7c9919dc4ec1d48c7f8ab1e447c4f2a3606
0xf0f <0x0meta@gmail.com>
2021-03-30 20:33:42 +1300

    remove File class
    provides very little benefit

--- codenode/__init__.py
+++ codenode/__init__.py
@@ -2,8 +2,6 @@ from .node import Node
 from .node import Line
 from .block import Block
 from .empty_lines import EmptyLines
-from .file import File
-
 default_indent = '    '
 default_writer = CodeNodeWriter()
 
