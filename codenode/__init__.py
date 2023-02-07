19a811d0427e9143a909a33aa2042f0e09227f32
0xf0f <0x0meta@gmail.com>
2023-02-08 05:11:45 +1300

    updated public facing module api

--- codenode/__init__.py
+++ codenode/__init__.py
@@ -1,3 +1,54 @@
-from .node import Node
-from .node import Line
-from .empty_lines import EmptyLines
+from .writer import Writer
+from .nodes.depth_change import RelativeDepthChange
+from .nodes.indentation import CurrentIndentation
+from .nodes.newline import Newline
+
+
+indent = RelativeDepthChange(1)
+dedent = RelativeDepthChange(-1)
+indentation = CurrentIndentation()
+newline = Newline()
+
+
+def line(content: str):
+    return indentation, content, newline
+
+
+def empty_lines(count: int):
+    return (newline,) * count
+
+
+def dump(
+        node, stream, *,
+        indentation='    ',
+        newline='\n',
+        depth=0,
+):
+    return Writer(
+        node,
+        indentation=indentation,
+        newline=newline,
+        depth=depth,
+    ).dump(stream)
+
+
+def dumps(
+        node, *,
+        indentation='    ',
+        newline='\n',
+        depth=0,
+):
+    return Writer(
+        node,
+        indentation=indentation,
+        newline=newline,
+        depth=depth,
+    ).dumps()
+
+
+__all__ = [
+    'indent', 'dedent',
+    'indentation', 'newline',
+    'line', 'empty_lines',
+    'dump', 'dumps'
+]
