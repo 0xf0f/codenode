4f962e26d5e1d2b0057386330f05553b1e5554a8
0xf0f <0x0meta@gmail.com>
2023-02-09 10:01:58 +1300

    moved all node processing logic into process_node
    the goal of this is to make Writer more extensible by allowing the handling of any node type to be altered, including iterables
    
    Writer instances also each now have a dedicated stack attribute that can be inspected i.e. in the case of error handling
    
    also added a parameter to auto coerce unhandled nodes to strings

--- codenode/writer.py
+++ codenode/writer.py
@@ -2,10 +2,10 @@ import typing
 
 from .nodes.newline import Newline
 from .nodes.indentation import Indentation
-from .nodes.depth_change import DepthChange, DepthChangePush, DepthChangePop
+from .nodes.depth_change import DepthChange
 
 if typing.TYPE_CHECKING:
-    from typing import Union, Iterable
+    from typing import Union, Iterable, Iterator
     NodeType = Iterable[Union[str, 'NodeType']]
 
 
@@ -15,41 +15,57 @@ class Writer:
             node: 'NodeType', *,
             indentation='    ',
             newline='\n',
-            depth=0
+            depth=0,
+            auto_coerce=False,
     ):
         self.node = node
+        self.stack: list['Iterator'] = list()
+
         self.indentation = indentation
         self.newline = newline
         self.depth = depth
 
-    def node_to_text(self, node: 'NodeType') -> 'Iterable[str]':
-        stack = [iter((node,))]
-        while stack:
+        self.auto_coerce = auto_coerce
+
+    def get_text_chunks(self) -> 'Iterable[str]':
+        self.stack.clear()
+        self.stack.append(iter((self.node,)))
+
+        while self.stack:
             try:
-                node = next(stack[-1])
+                node = next(self.stack[-1])
             except StopIteration:
-                stack.pop()
+                self.stack.pop()
                 continue
 
-            for node in self.process_node(node):
-                if isinstance(node, str):
-                    yield node
-                else:
-                    stack.append(iter(node))
+            yield from self.process_node(node)
 
-    def process_node(self, node):
-        if isinstance(node, DepthChange):
+    def process_node(self, node) -> 'Iterable[str]':
+        if isinstance(node, str):
+            yield node
+        elif isinstance(node, DepthChange):
             self.depth = node.new_depth_for(self.depth)
         elif isinstance(node, Indentation):
             yield self.indentation * node.indents_for(self.depth)
         elif isinstance(node, Newline):
             yield self.newline
         else:
-            yield node
+            try:
+                self.stack.append(iter(node))
+            except TypeError:
+                if self.auto_coerce:
+                    yield str(node)
+                else:
+                    raise TypeError(
+                        f'Unable to process node "{node}".\n'
+                        'Either convert it to a string, iterable or '
+                        'override Writer.process_node to handle nodes '
+                        'of this type.'
+                    )
 
     def dump(self, stream):
-        for chunk in self.node_to_text(self.node):
+        for chunk in self.get_text_chunks():
             stream.write(chunk)
 
     def dumps(self):
-        return ''.join(self.node_to_text(self.node))
+        return ''.join(self.get_text_chunks())
