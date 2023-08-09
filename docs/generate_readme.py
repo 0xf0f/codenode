955fdc3ff0ba0553209f031b20034b0968220b22
0xf0f <0x0meta@gmail.com>
2023-08-10 05:15:10 +1200

    added readme generator

---
+++ docs/generate_readme.py
@@ -0,0 +1,421 @@
+import ast
+import importlib
+import inspect
+import functools
+import re
+import textwrap
+import types
+import typing
+
+import codenode_utilities.prefixer
+from codenode import line, dumps, dump
+
+docstring_regex = re.compile(
+    r'(?P<param>:param (?P<param_name>.+): (?P<param_description>[^:]+))|'
+    r'(?P<return>:return[s]?: (?P<return_description>[^:]+))',
+    re.MULTILINE,
+)
+
+# class_attributes_regex = re.compile(
+#     r"self\.(?P<name>\S+)\s*(\s*:\s+(?P<type>.*?)\s+)?=.*?"
+#     r"(?P<quote>[\"']{3}|[\"'])(?P<description>.*?)(?P=quote)",
+#     re.MULTILINE | re.DOTALL,
+# )
+
+
+def dedent_lines(lines: str):
+    return textwrap.dedent(lines)
+
+
+def get_docstring_info(docstring):
+    result = {
+        'summary': '',
+        'params': [],
+        'return': '',
+    }
+
+    matches = (*docstring_regex.finditer(docstring),)
+
+    for match in matches:
+        result['summary'] = dedent_lines(docstring[:match.start()])
+        break
+    else:
+        result['summary'] = dedent_lines(docstring)
+
+    for match in matches:
+        groups = match.groupdict()
+        if groups.get('param', ''):
+            result['params'].append(
+                {
+                    'name': groups['param_name'],
+                    'description': dedent_lines(groups['param_description']),
+                }
+            )
+        if groups.get('return', ''):
+            result['return'] = dedent_lines(groups['return_description'])
+    return result
+
+
+def get_function_documentation(function: types.FunctionType):
+    return {
+        'name': function.__name__,
+        'signature': f'def {function.__name__}{inspect.signature(function)}',
+        **get_docstring_info(function.__doc__),
+    }
+
+
+def get_class_attributes_documentation(cls: typing.Type):
+    if '__init__' in vars(cls):
+        # print('attributes', cls)
+        source = textwrap.dedent(inspect.getsource(cls.__init__))
+
+        # return [
+        #     {
+        #         'name': match.group('name'),
+        #         'description': match.group('description'),
+        #         'type': match.group('type')
+        #     }
+        #     for match in class_attributes_regex.finditer(source)
+        # ]
+
+        node = ast.parse(source)
+        for child in ast.iter_child_nodes(node):
+            if isinstance(child, ast.FunctionDef):
+                for a, b in zip(child.body, child.body[1:]):
+                    if (
+                        isinstance(b, ast.Expr) and
+                        isinstance(b.value, ast.Constant) and
+                        isinstance(b.value.value, str)
+                    ):
+                        print(a, b)
+                        if isinstance(a, ast.Assign):
+                            for target in a.targets:
+                                if (
+                                    isinstance(target, ast.Attribute) and
+                                    isinstance(target.value, ast.Name) and
+                                    target.value.id == 'self'
+                                ):
+                                    yield {
+                                        'name': target.attr,
+                                        'description': dedent_lines(b.value.value.strip()),
+                                        'type': '',
+                                    }
+                        elif isinstance(a, ast.AnnAssign):
+                            if (
+                                    isinstance(a.target, ast.Attribute) and
+                                    isinstance(a.target.value, ast.Name) and
+                                    a.target.value.id == 'self'
+                            ):
+                                yield {
+                                    'name': a.target.attr,
+                                    'description': dedent_lines(b.value.value.strip()),
+                                    'type': ast.unparse(a.annotation)
+                                }
+
+
+def get_module_attribute_documentation(module: types.ModuleType):
+    try:
+        attribute_docs = getattr(module, '__attribute_docs__')
+    except AttributeError:
+        source = inspect.getsource(module)
+        node = ast.parse(source)
+        attribute_docs = dict()
+
+        for a, b in zip(node.body, node.body[1:]):
+            if (
+                    isinstance(b, ast.Expr) and
+                    isinstance(b.value, ast.Constant) and
+                    isinstance(b.value.value, str)
+            ):
+                if isinstance(a, ast.Assign):
+                    for target in a.targets:
+                        if isinstance(target, ast.Name):
+                            attribute_docs[target.id] = {
+                                'description': b.value.value,
+                                'type': '',
+                            }
+                elif isinstance(a, ast.AnnAssign):
+                    if isinstance(a.target, ast.Name):
+                        attribute_docs[a.target.id] = {
+                            'description': b.value.value,
+                            'type': ast.unparse(a.annotation),
+                        }
+        setattr(module, '__attribute_docs__', attribute_docs)
+    return attribute_docs
+
+
+def get_class_documentation(cls: typing.Type):
+    print(cls)
+    return {
+        'name': cls.__name__,
+        'summary': dedent_lines(cls.__doc__ or ''),
+        'methods': [
+            get_function_documentation(member)
+            for member in vars(cls).values()
+            if isinstance(member, types.FunctionType)
+            and member.__doc__
+        ],
+        'attributes': tuple(get_class_attributes_documentation(cls)),
+    }
+
+
+from codenode import dumps, dump, Writer
+print(get_class_documentation(Writer))
+
+
+# class ModuleDocumentation:
+#     pass
+#
+#
+
+class DocumentationContent:
+    def __init__(self, path: str):
+        self.path = path
+
+    # def get_source_link(self):
+    #     obj = self.get_value()
+    #     lines = inspect.getsourcelines(obj)
+    #     file = inspect.getsourcefile(obj)
+    #     return '[View Source]()'
+
+    @functools.cache
+    def get_module(self):
+        module_path, _, attribute = self.path.rpartition('.')
+        return importlib.import_module(module_path)
+
+    @functools.cache
+    def get_value(self):
+        module_path, _, attribute = self.path.rpartition('.')
+        module = importlib.import_module(module_path)
+        print(module_path, attribute)
+        return getattr(module, attribute)
+
+    def get_documentation(self):
+        yield line(f'### {self.path}')
+        yield newline
+        # yield line('---')
+
+    @functools.cache
+    def get_link(self):
+        return '-'.join(
+            re.sub(
+                r'[^A-Za-z]+', ' ',
+                self.path.lower()
+            ).strip().split()
+        )
+
+    def __iter__(self):
+        return self.get_documentation()
+
+    def __repr__(self):
+        return f'<Documentation {self.path}>'
+
+
+from codenode import line, dumps, newline, lines, indent, dedent, indentation
+
+quote_block = codenode_utilities.prefixer('> ')
+
+
+def function_param_docs(params: dict):
+    for param in params:
+        yield line(''.join((
+            f'* > ***{param["name"]}',
+            # f' ({param["type"]})' if param["type"] else "",
+            f':*** ',
+        )))
+        yield (
+            line(f'  > {line_content}')
+            for line_content in param["description"].splitlines()
+        )
+
+
+class ClassDocumentation(DocumentationContent):
+    def get_documentation(self):
+        yield from super().get_documentation()
+
+        cls = self.get_value()
+        documentation = get_class_documentation(cls)
+        yield quote_block((
+            line('```python'),
+            line(f'class {cls.__name__}: ...'),
+            line('```'),
+            (
+                line(f'{line_content}')
+                for line_content in documentation['summary'].splitlines()
+            ),
+        ))
+
+        if documentation['methods']:
+            yield line('#### Methods')
+            for method in documentation['methods']:
+                yield quote_block((
+                    line(f'##### `{method["name"]}`'),
+                    # line('#### Summary'),
+                    tuple((
+                        line('```python'),
+                        line(f'class {cls.__name__}:'),
+                        indent,
+                        line(f'{method["signature"]}: ...'),
+                        dedent,
+                        line('````'),
+                        lines(*method['summary'].splitlines()),
+                    )),
+                    line(''),
+                    (
+                        line('#### Parameters'),
+                        *function_param_docs(method['params']),
+                    ) if method['params'] else (),
+                    (
+                        line('#### Returns'),
+                        line(f'* > {method["return"]}'),
+                    ) if method["return"] else (),
+                ))
+                yield newline
+
+        if documentation['attributes']:
+            yield line('#### Attributes')
+
+            for attribute in documentation['attributes']:
+                yield quote_block((
+                    line(
+                        f'***{attribute["name"]}:*** ' +
+                        (f'{attribute["type"]} - ' if attribute["type"] else "")
+                    ),
+                    *(
+                        line(f'{line_content}')
+                        for line_content in attribute["description"].splitlines()
+                    ),
+                ))
+                yield newline
+
+
+class FunctionDocumentation(DocumentationContent):
+    def get_documentation(self):
+        yield from super().get_documentation()
+
+        function = self.get_value()
+        documentation = get_function_documentation(function)
+
+        yield quote_block((
+            # line('#### Summary'),
+            tuple((
+                line('```python'),
+                line(f'{documentation["signature"]}: ...'),
+                line('````'),
+                lines(*documentation['summary'].splitlines()),
+            )),
+            line(''),
+            (
+                line('#### Parameters'),
+                *function_param_docs(documentation['params']),
+            ) if documentation['params'] else (),
+            (
+                line('#### Returns'),
+                line(f'* > {documentation["return"]}'),
+            ) if documentation["return"] else (),
+        ))
+        yield newline
+
+
+class AttributeDocumentation(DocumentationContent):
+    def get_documentation(self):
+        yield from super().get_documentation()
+        module, _, name = self.path.rpartition('.')
+        docstring = get_module_attribute_documentation(self.get_module()).get(name, '')
+        if docstring:
+            yield newline
+            yield quote_block(line(docstring['description']))
+            yield newline
+
+import functools
+
+functions = functools.partial(map, FunctionDocumentation)
+classes = functools.partial(map, ClassDocumentation)
+variables = functools.partial(map, AttributeDocumentation)
+
+
+contents = (
+    *functions(
+        (
+            'codenode.dump',
+            'codenode.dumps',
+            'codenode.line',
+        )
+    ),
+
+    *variables(
+        (
+            'codenode.indent',
+            'codenode.dedent',
+            'codenode.newline',
+            'codenode.indentation',
+        )
+    ),
+
+    *functions(
+        (
+            'codenode.lines',
+            'codenode.empty_lines',
+            'codenode.indented',
+        )
+    ),
+
+    AttributeDocumentation('codenode.default_writer_type'),
+
+    *classes(
+        (
+            'codenode.writer.Writer',
+            'codenode.writer.WriterStack',
+
+            'codenode.nodes.newline.Newline',
+
+            'codenode.nodes.depth_change.DepthChange',
+            'codenode.nodes.depth_change.RelativeDepthChange',
+            'codenode.nodes.depth_change.AbsoluteDepthChange',
+
+            'codenode.nodes.indentation.Indentation',
+            'codenode.nodes.indentation.RelativeIndentation',
+            'codenode.nodes.indentation.AbsoluteIndentation',
+            'codenode.nodes.indentation.CurrentIndentation',
+
+            # 'codenode.debug.DebugIterator',
+        )
+    ),
+
+    *functions(
+        (
+            # 'codenode.debug.print_writer_stack',
+            'codenode.debug.debug_patch',
+        )
+    ),
+)
+
+
+def reference_contents():
+    for item in contents:
+        yield line(f'- [{item.path}](#{item.get_link()})')
+
+
+def reference():
+    for item in contents:
+        yield line('---')
+        yield item
+
+import string
+import pathlib
+
+if __name__ == '__main__':
+    # print(parse(dumps.__doc__).returns.description)
+    cd = pathlib.Path(__file__).absolute().parent
+    with open(cd / 'readme_template.md') as file:
+        template = string.Template(file.read())
+
+    with open(cd.parent / 'README.md', 'w') as file:
+        file.write(
+            template.safe_substitute(
+                {
+                    'reference_contents': dumps(reference_contents(), debug=True),
+                    'reference': dumps(reference(), debug=True),
+                }
+            )
+        )
---
+++ docs/readme_template.md
@@ -0,0 +1,172 @@
+### Contents
+- [What is this?](#what-is-this)
+- [How do I install it?](#how-do-i-install-it)
+- [How do I use it?](#how-do-i-use-it)
+- [Extensions](#extensions)
+- [Reference](#reference)
+
+### What is this?
+The goal of this module is to help write code that generates code. 
+Focus is placed on enabling the user to easily describe, 
+build and reason about code structures rapidly.
+
+### How do I install it?
+
+[comment]: <> (### From PyPI:)
+
+[comment]: <> (`pip install 0xf0f-codenode`)
+
+#### From GitHub:
+`pip install git+https://github.com/0xf0f/codenode`
+
+### How do I use it?
+Like the `json` and 
+`pickle` modules, `dump` and `dumps` are used to generate output. 
+Code can be built using any tree of iterables containing strings, 
+indentation nodes and newline nodes.
+
+For example, the built-in `line` function returns a tuple:
+
+```python
+from codenode import indentation, newline
+
+def line(content):
+    return indentation, content, newline
+```
+
+Which we can combine with `indent` and `dedent` nodes:
+
+```python
+from codenode import line, indent, dedent, dumps
+
+def counting_function(count_from, count_to):
+    return [
+        line(f'def count_from_{count_from}_to_{count_to}():'),
+        indent,
+        [
+            line(f'print({i})')
+            for i in range(count_from, count_to)
+        ],
+        dedent,
+    ]
+
+print(dumps(counting_function(0, 5)))
+```
+
+Which outputs:
+```
+def count_from_0_to_5():
+    print(0)
+    print(1)
+    print(2)
+    print(3)
+    print(4)
+```
+
+But what if we want to count to a really big number, like 
+1,000,000,000,000,000?
+It would be inefficient to store all those lines in memory
+at once. We can use a generator to break them down into 
+individual parts instead:
+
+```python
+from codenode import indent, dedent, newline, indentation, dump
+
+def counting_function_generator(count_from, count_to):
+    yield indentation 
+    yield 'def count_from_', str(count_from), '_to_', str(count_to), '():'
+    yield newline
+    
+    yield indent
+    for i in range(count_from, count_to):
+        yield indentation, 'print(', str(i), ')', newline
+    yield dedent
+
+with open('code.py', 'w') as file:
+    dump(counting_function_generator(0, 1_000_000_000_000_000), file)
+```
+
+We can also build a class with an `__iter__` method:
+
+```python
+from codenode import line, indent, dedent, dump
+
+class CountingFunction:
+    def __init__(self, count_from, count_to):
+        self.count_from = count_from
+        self.count_to = count_to
+    
+    def __iter__(self):
+        yield line(
+            f'def count_from_{self.count_from}_to_{self.count_to}():'
+        )
+        yield indent
+        for i in range(self.count_from, self.count_to):
+            yield line(f'print({i})')
+        yield dedent
+
+with open('code.py', 'w') as file:
+    dump(CountingFunction(0, 1_000_000), file)
+```
+
+Or a more generalized function class:
+
+```python
+class Function:
+    def __init__(self, name, *args):
+        self.name = name
+        self.args = args
+        self.children = []
+    
+    def __iter__(self):
+        arg_string = ', '.join(self.args)
+        yield line(f'def {self.name}({arg_string}):')
+        yield indent
+        yield self.children
+        yield dedent
+
+class CountingFunction(Function):
+    def __init__(self, count_from, count_to):
+        super().__init__(f'count_from_{count_from}_to_{count_to}')
+        for i in range(count_from, count_to):
+            self.children.append(line(f'print({i})'))
+```
+
+Leveraging python's iteration protocol like this allows:
+- Mixing and matching whatever fits the use case to maximize tradeoffs, 
+such as using generators for their memory efficiency, 
+custom iterable classes for their semantics, or plain old lists and 
+tuples for their simplicity.
+- Taking advantage of existing modules that offer tooling for 
+iterables, such as itertools.
+- Building higher level structures from as many iterable building blocks
+as desired.
+
+### Extensions
+Module behaviour can be extended by overriding methods of the 
+`codenode.writer.Writer` and `codenode.writer.WriterStack` classes. An
+example of this can be seen in the `codenode.debug.debug_patch` 
+function. The variable `codenode.default_writer_type` can be used to
+replace the `Writer` type used in `dump` and `dumps` with a custom one.
+
+Some modules with helper classes and functions are also provided:
+  - codenode_utilities
+    - contains general language agnostic helper functions and classes
+  - codenode_python
+    - contains helper classes and functions for generating python code
+  - codenode_cpp
+    - contains helper classes and functions for generating c++ code
+  - codenode_legacy
+    - helpers for code that relies on the old codenode API (below version 1.0)
+    - uses a previous, entirely different approach to nodes
+
+### Reference
+> **Note**
+> This section of the readme was generated using codenode itself.
+> 
+> See docs/generate_readme.py
+
+#### Contents
+${reference_contents}
+
+${reference}
