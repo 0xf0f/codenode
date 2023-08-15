83fc2e3840eb7c07af0204b855ff8751142cb2f5
0xf0f <0x0meta@gmail.com>
2023-08-16 08:29:14 +1200

    added alternative readme generator for pypi

--- docs/generate_pypi_readme.py
+++ docs/generate_pypi_readme.py
@@ -8,7 +8,7 @@ from docs.generation_utilities import (
 
 cd = pathlib.Path(__file__).absolute().parent
 template_path = cd / 'readme_template.md'
-output_path = cd.parent / 'README.md'
+# output_path = cd.parent / 'README_pypi.md'
 
 contents = (
     *functions(
@@ -78,16 +78,25 @@ def reference():
         yield item
 
 
-if __name__ == '__main__':
+def run():
     with open(template_path) as file:
         template = string.Template(file.read())
 
-    with open(output_path, 'w') as file:
-        file.write(
-            template.safe_substitute(
-                {
-                    'reference_contents': codenode.dumps(reference_contents(), debug=True),
-                    'reference': codenode.dumps(reference(), debug=True),
-                }
+    output = template.safe_substitute(
+        {
+            'reference_contents': codenode.dumps(reference_contents(), debug=True),
+            'reference': codenode.dumps(reference(), debug=True),
+            'codenode_utilities_link': (
+                'https://github.com/0xf0f/codenode/'
+                'blob/main/codenode_utilities/README.md'
             )
-        )
+        }
+    )
+    # with open(output_path, 'w') as file:
+    #     file.write(output)
+
+    return output
+
+
+if __name__ == '__main__':
+    run()
--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+import sys
+
 from setuptools import setup, find_packages
+
+if sys.version_info[1] < 9:
+    # readme generation uses some ast features only available in 3.9+
+    readme = (
+        "A simple framework for code generation. "
+        "View project info at https://github.com/0xf0f/codenode"
+    )
+else:
+    from docs import generate_pypi_readme
+    readme = generate_pypi_readme.run()
 
 setup(
     name='codenode',
@@ -8,5 +20,7 @@ setup(
     license='MIT',
     author='0xf0f',
     author_email='0x0meta@gmail.com',
-    description='a simple framework for code generation'
+    description='a simple framework for code generation',
+    long_description=readme,
+    long_description_content_type='text/markdown',
 )
