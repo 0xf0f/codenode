0a9929d0c66e95180c4fed44be9d114fbda3ae86
0xf0f <0x0meta@gmail.com>
2023-08-16 08:32:33 +1200

    added classifiers and minimum version

--- setup.py
+++ setup.py
@@ -27,4 +27,10 @@ setup(
     description='a simple framework for code generation',
     long_description=readme,
     long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
