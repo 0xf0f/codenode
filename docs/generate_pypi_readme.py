3848eb7056dc8af65a470df944a7d10e22603831
0xf0f <0x0meta@gmail.com>
2023-08-17 08:01:36 +1200

    reuse methods from generate_readme in generate_pypi_readme

--- docs/generate_pypi_readme.py
+++ docs/generate_pypi_readme.py
@@ -2,80 +2,11 @@ import codenode
 import pathlib
 import string
 
-from docs.generation_utilities import (
-    functions, classes, variables, AttributeDocumentation
-)
+from docs.generate_readme import reference, reference_contents
 
 cd = pathlib.Path(__file__).absolute().parent
 template_path = cd / 'readme_template.md'
 # output_path = cd.parent / 'README_pypi.md'
-
-contents = (
-    *functions(
-        (
-            'codenode.dump',
-            'codenode.dumps',
-            'codenode.line',
-        )
-    ),
-
-    *variables(
-        (
-            'codenode.indent',
-            'codenode.dedent',
-            'codenode.newline',
-            'codenode.indentation',
-        )
-    ),
-
-    *functions(
-        (
-            'codenode.lines',
-            'codenode.empty_lines',
-            'codenode.indented',
-        )
-    ),
-
-    AttributeDocumentation('codenode.default_writer_type'),
-
-    *classes(
-        (
-            'codenode.writer.Writer',
-            'codenode.writer.WriterStack',
-
-            'codenode.nodes.newline.Newline',
-
-            'codenode.nodes.depth_change.DepthChange',
-            'codenode.nodes.depth_change.RelativeDepthChange',
-            'codenode.nodes.depth_change.AbsoluteDepthChange',
-
-            'codenode.nodes.indentation.Indentation',
-            'codenode.nodes.indentation.RelativeIndentation',
-            'codenode.nodes.indentation.AbsoluteIndentation',
-            'codenode.nodes.indentation.CurrentIndentation',
-
-            # 'codenode.debug.DebugIterator',
-        )
-    ),
-
-    *functions(
-        (
-            # 'codenode.debug.print_writer_stack',
-            'codenode.debug.debug_patch',
-        )
-    ),
-)
-
-
-def reference_contents():
-    for item in contents:
-        yield codenode.line(f'- [{item.path}](#{item.get_link()})')
-
-
-def reference():
-    for item in contents:
-        yield codenode.line('---')
-        yield item
 
 
 def run():
